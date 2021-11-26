8be7684830ee6686ff8b3aaa353854672e3e99c0
csl <3079625093@qq.com>
2021-11-26 23:50:54 +0800

    small change

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include "point.hpp"
+#include "triangle.hpp"
 #include <list>
 
-using namespace ns_point;
+using namespace ns_geo;
 
 void foo_point2()
 {
@@ -78,7 +79,7 @@ void foo_point3()
     return;
 }
 
-void foo_cast_test()
+void foo_ponitCast_test()
 {
     Point3f p(1, 2, 6);
     Point2f p2(2, 6);
@@ -94,10 +95,45 @@ void foo_cast_test()
     return;
 }
 
+void foo_triangle2()
+{
+    // construct the triangle-2d
+    ns_geo::Point2<double> points[3] = {
+        Point2d(0, 0),
+        Point2d(2, 2),
+        Point2d(2, 0)};
+    ns_geo::Triangle2d tri(points);
+    tri = ns_geo::Triangle2d(points[0], points[1], points[2]);
+    tri = ns_geo::Triangle2d(Point2d(0, 0), Point2d(2, 2), Point2d(2, 0));
+    // output
+    std::cout << tri << std::endl;
+    // calculate the area and perimeter
+    std::cout << "area : " << tri.area() << std::endl;
+    std::cout << "perimeter : " << tri.perimeter() << std::endl;
+    return;
+}
+
+void foo_triangle3()
+{
+    // construct the triangle-3d
+    ns_geo::Point3<double> points[3] = {
+        Point3d(0, 0, 0),
+        Point3d(2, 2, 2),
+        Point3d(2, 0, 0)};
+    ns_geo::Triangle3d tri(points);
+    tri = ns_geo::Triangle3d(points[0], points[1], points[2]);
+    tri = ns_geo::Triangle3d(Point3d(0, 0, 0), Point3d(2, 2, 2), Point3d(2, 0, 0));
+    // output
+    std::cout << tri << std::endl;
+    // calculate the area and perimeter
+    std::cout << "area : " << tri.area() << std::endl;
+    std::cout << "perimeter : " << tri.perimeter() << std::endl;
+    return;
+}
+
 int main(int argc, char *argv[])
 {
-    ::foo_point2();
-    ::foo_point3();
-    ::foo_cast_test();
+    ::foo_triangle2();
+    ::foo_triangle3();
     return 0;
 }
