89abbefd268206645a99aa14da50e9282c3310a2
csl <3079625093@qq.com>
2021-12-06 12:31:47 +0800

    small change

--- main.cpp
+++ main.cpp
@@ -9,16 +9,21 @@ using namespace ns_geo;
 
 void foo_polygon()
 {
-    Polygond polygon({Point2d(0, 0),
-                      Point2d(0, 1),
-                      Point2d(0.5, 2),
-                      Point2d(1, 1),
-                      Point2d(1, 0)});
+    RefPointSet2d rps;
+    rps.insert({0, RefPoint2d::ary_type{0, 0}});
+    rps.insert({1, RefPoint2d::ary_type{1, 0}});
+    rps.insert({2, RefPoint2d::ary_type{1, 1}});
+    rps.insert({4, RefPoint2d::ary_type{0, 1}});
+
+    auto polygon = rps.createRefPolygon({2, 1, 0, 4});
     std::cout << polygon << std::endl;
+    std::cout << "first refPoint : " << polygon.indexAt(0) << std::endl;
+    std::cout << "first refPoint : " << polygon.idAt(2) << std::endl;
     std::cout << "perimeter : " << polygon.perimeter() << std::endl;
     std::cout << "area : " << polygon.area() << std::endl;
     return;
 }
+
 int main(int argc, char *argv[])
 {
     ::foo_polygon();
