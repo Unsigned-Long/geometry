ef10619e24ff79937d0cccfc1fe5855a10fa779f
csl <3079625093@qq.com>
2021-12-11 21:44:32 +0800

    add kdtree

--- main.cpp
+++ main.cpp
@@ -13,29 +13,25 @@
 #include "rectangle.hpp"
 #include "polygon.hpp"
 #include "linestring.hpp"
+#include "kdtree.hpp"
 
 using namespace ns_geo;
 
-void foo_distance()
+void foo_kdtree()
 {
-    Point2d p1(1, 1);
-    Point2d p2(2, 2);
-    Line2d line({0, 0, 0, 1});
-    std::cout << "p1 -> p2 : " << distance(p1, p2) << std::endl;
-    std::cout << "p1 -> line : " << distance(p1, line) << std::endl;
-    double ary2[2] = {2, 3};
-    RefPointSet2d rps;
-    rps.insert({0, RefPoint2d::ary_type{0, 0}});
-    rps.insert({1, ary2});
-    rps.insert({2, RefPoint2d::ary_type{0, 0}});
-    rps.insert({4, ary2});
-    auto refline = rps.createRefLine2(0, 1);
-    std::cout << "p1 -> refline : " << distance(p1, Line2d(refline)) << std::endl;
+    PointSet3f ps({{3, 1, 4},
+                   {2, 3, 7},
+                   {2, 0, 3},
+                   {2, 4, 5},
+                   {1, 4, 4},
+                   {0, 5, 7}});
+    KdTree3<Point3f> kdtree(ps);
+    kdtree.printKdTree(1);
     return;
 }
 
 int main(int argc, char *argv[])
 {
-    ::foo_distance();
+    ::foo_kdtree();
     return 0;
 }
