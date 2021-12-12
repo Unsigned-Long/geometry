1c860b640ff510d6eca614030c04142cdda1e428
csl <3079625093@qq.com>
2021-12-12 22:00:48 +0800

    add nearest k search

--- pyDrawer/kdtree/kdtree.py
+++ pyDrawer/kdtree/kdtree.py
@@ -5,9 +5,11 @@ import csv
 import numpy as np
 import math
 
-opname = './originPoints.csv'
-spname = './searchPoints.csv'
+opname = './nearest1.csv'
+spname = './nearest2.csv'
 
+# opname = './radius1.csv'
+# spname = './radius2.csv'
 
 def readOPoints(filename):
     x = []
@@ -18,7 +20,6 @@ def readOPoints(filename):
             x.append(float(point[0]))
             y.append(float(point[1]))
     return x, y
-
 
 def readSPoints(filename):
     x = []
@@ -35,23 +36,31 @@ def readSPoints(filename):
 
 opx, opy = readOPoints(opname)
 spx, spy, spd = readSPoints(spname)
+tp = [0, 0]
 
 plt.rcParams["font.family"] = "Ubuntu Mono"
 plt.rcParams['font.size'] = 13
 
+for i in range(len(spx)):
+    plt.plot([tp[0], spx[i]], [tp[1], spy[i]], c='g', linestyle='--')
+
 plt.scatter(opx, opy, label='origin points', c='r', marker='X')
 plt.scatter(spx, spy, label='searched points', c='g', s=spd)
-plt.scatter(53, 25, label='target point', c='b', marker='^')
+# plt.scatter(spx, spy, label='searched points', c='g')
+plt.scatter(tp[0], tp[1], label='target point', c='b', marker='^', s=100)
 
-alpha = np.linspace(0.0, 2.0*math.pi, 100)
-x = 65.0*np.cos(alpha) + 53
-y = 65.0*np.sin(alpha) + 25
-plt.plot(x, y, linestyle='--')
+
+# alpha = np.linspace(0.0, 2.0*math.pi, 100)
+# x = 50.0*np.cos(alpha) + 0
+# y = 50.0*np.sin(alpha) + 0
+# plt.plot(x, y, linestyle='--')
 
 plt.xlabel('X(m)')
 plt.ylabel('Y(m)')
 plt.grid(ls='--', alpha=0.5)
-plt.title('Radius Search on Kd-Tree [radius = 65.0]')
+
+# plt.title('Radius Search on Kd-Tree [radius = 65.0]')
+plt.title('K Nearest Search on Kd-Tree [K = 5]')
 
 plt.legend()
 
